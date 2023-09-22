4cfb3c265b165208ecfecedd5e154ad08936ff0e
AsZaychik <aszaychik@gmail.com>
2023-09-23 06:13:05 +0700

    HTTP Server

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"Aszaychik/belajar-restful-api/app"
 	"Aszaychik/belajar-restful-api/controller"
+	"Aszaychik/belajar-restful-api/helper"
 	"Aszaychik/belajar-restful-api/repository"
 	"Aszaychik/belajar-restful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/julienschmidt/httprouter"
@@ -26,4 +30,11 @@ func main() {
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
 
+	server := http.Server{
+		Addr: "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 }
